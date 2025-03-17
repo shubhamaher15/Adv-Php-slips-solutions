60a74a9a43bbcb9b73d6884e92b932cceca0fe9d
Shubham Aher <ahershubham055@gmail.com>
2025-03-17 21:15:14 +0530

    ad.php assinment 2/assinment 2

---
+++ ad.php assinment 2/q9.php
@@ -0,0 +1,20 @@
+<html>
+   <body>
+     <form method="post" action="#">
+         Enter string1:
+           <input type="text"  name="t1" value="<?php  echo($_POST["t1"]);?>"><br>
+       Enter string2:
+          <input type="text" name="t2" value="<?php  echo($_POST["t2"]);?>"><br>
+       <input type="submit" value="ok"> 
+   </form>
+  </body>
+</html>
+<?php 
+     $s1=$_POST["t1"];
+     $s2=$_POST["t2"];
+       if($s1==$s2)
+           echo("<br> string are same");
+       else
+           echo("<br> string are not same");
+?>
+  
